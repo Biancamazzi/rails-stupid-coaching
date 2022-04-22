be8e67875b18a73fc8d981e056f9de242437bc7d
BiancaMazzi <bianca.mazzi@hotmail.com>
2022-04-21 22:48:40 -0300

    final

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,17 @@
 # comment
 class QuestionsController < ApplicationController
-  def action
-    puts 'action'
+  def ask
+    puts 'ask'
   end
 
   def answer
-    puts 'answer'
+    @ask = params['question']
+    if @ask == 'I am going to work'
+      @answer = 'Great!'
+    elsif @ask.ends_with? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
   end
 end
